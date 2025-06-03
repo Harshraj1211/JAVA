e89fd4a6d12a37ef0337f515e746358e72a7ded9
Harshrajsinh Gohil <harshrajsinh573@gmail.com>
2025-06-03 19:07:27 +0530

    Add files via upload

---
+++ LAB 1/lab1_a3.java
@@ -0,0 +1,7 @@
+public class lab1_a3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hellow world.");
+	}
+}
---
+++ LAB 1/lab1_c6.java
@@ -0,0 +1,9 @@
+public class lab1_c6 {
+    public static void main(String[] args) {
+        // Loop through the command line arguments
+        for (String arg : args) {
+            // Print each argument
+            System.out.println("Argument: " + arg);
+        }
+    }
+}
