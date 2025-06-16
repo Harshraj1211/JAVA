164e1344989916a9c5708de42c2a7a41e1f5fad6
Harshrajsinh Gohil <harshrajsinh573@gmail.com>
2025-06-16 09:29:35 +0530

    Add files via upload

---
+++ lab 8/lab8_a2.java
@@ -0,0 +1,25 @@
+// Write a program that illustrates interface inheritance. Interface A is extended by A1 and 
+// A2. Interface A12 inherits from both A1 and A2.Each interface declares one constant 
+// and one method. Class B implements A12.Instantiate B and invoke each of its methods. 
+// Each method displays one of the constants. [A]
+
+import java.util.*;
+
+interface A {
+    
+}
+
+interface A1 extends A{
+    
+}
+
+interface A2 extends A{
+    
+}
+
+public class lab8_a2 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    }
+}
