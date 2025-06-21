db0b5ca0eae510667c23cc83a0ca6b4f66790b70
Harshrajsinh Gohil <harshrajsinh573@gmail.com>
2025-06-21 21:34:21 +0530

    Add files via upload

---
+++ DemoPackage/PackageDemo1.java
@@ -0,0 +1,7 @@
+package DemoPackage;
+import java.util.*;
+public class PackageDemo1 {
+    public void PrintStatement(){
+        System.out.println("Hellowwww!!! i'm inside of package demo 1.");
+    }
+}
---
+++ DemoPackage/PackageDemo2.java
@@ -0,0 +1,9 @@
+package DemoPackage;
+import java.util.*;
+public class PackageDemo2 {
+    public static void main(String[] args) {
+        PackageDemo1 pd1=new PackageDemo1();
+
+        pd1.PrintStatement();
+    }
+}
